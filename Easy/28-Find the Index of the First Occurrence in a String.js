dc54eba7a0811c57fc6ab0b62b48c7fbb9b5aa2c
Hossam Mahmoud <76559078+HossamMahmoudkhedr@users.noreply.github.com>
2024-06-09 18:21:35 -0700

    Update 28-Find the Index of the First Occurrence in a String.js

--- Easy/28-Find the Index of the First Occurrence in a String.js
+++ Easy/28-Find the Index of the First Occurrence in a String.js
@@ -1,1 +1,25 @@
-
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let occurrence = -1;
+    for(let i = 0; i<haystack.length; i++){
+        if(needle[0] === haystack[i]){
+            occurrence = i;
+            
+            for(let j = 1;j<needle.length;j++){
+                if(needle[j] !== haystack[i+j]){
+                    occurrence = -1;
+                    break;
+                }
+            }
+            
+            if(occurrence !== -1){
+                break;
+            }
+        }
+    }
+    return occurrence;
+};
