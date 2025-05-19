e6f2b9be522b37496e5174006bfc87c3dffbccdf
Hossam Mahmoud <76559078+HossamMahmoudkhedr@users.noreply.github.com>
2025-05-20 02:58:13 +0300

    Update 283. Move Zeroes.cpp

--- Easy/283. Move Zeroes.cpp
+++ Easy/283. Move Zeroes.cpp
@@ -18,3 +18,27 @@ public:
         }
     }
 };
+
+
+// Better Solution [O(n) Time Complexity]
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        int count =0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                count++;
+            }else{
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        
+        while(count){
+            nums[j] = 0;
+            j++;
+            count--;
+        }
+    }
+};
